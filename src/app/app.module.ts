9b9a1d704b41ff60789545218eb91379c2123060
Noah Henry <noah.henry@allscripts.com>
2019-07-31 10:10:07 -0500

    Getting started...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ArtistItemsComponent } from './artist-items/artist-items.component';
-import { SearchArtistsPipe } from './search-artists.pipe';
-import { ArtistDetailsComponent } from './artist-details/artist-details.component';
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ArtistItemsComponent,
-    SearchArtistsPipe,
-    ArtistDetailsComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule, FormsModule, HttpClientModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
